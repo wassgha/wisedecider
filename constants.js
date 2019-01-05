696ed0807d555a89e4e9726389c3d31b682476ac
Wassim Gharbi <wassgha@gmail.com>
2019-01-05 18:55:40 +0100

    Fixed server host error

--- constants.js
+++ constants.js
@@ -8,4 +8,5 @@ export const BLOCK = {
 export const LOCAL_DB = 'nextjs-express-boilerplate'
 export const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost/${LOCAL_DB}`
 export const PORT = process.env.PORT || 3001
-export const SERVER_HOST = process.env.SERVER_HOST || `http://localhost:${PORT}/`
+export const SERVER_HOST =
+  process.env.SERVER_HOST || `https://wisedecider.herokuapp.com/` || `http://localhost:${PORT}/`
