16d26a1944de7d0fd3f13b205830202ca1e2e8e4
Wassim Gharbi <wassgha@gmail.com>
2019-03-06 23:45:51 -0500

    Heroku fix

--- keys.js
+++ keys.js
@@ -1,15 +1,19 @@
-const dotenv = require('dotenv').config()
+const dotenv = process.env.NODE_ENV !== 'PROD' ? require('dotenv').config() : {}
 
 if (dotenv.error) {
   throw dotenv.error
 }
 
-const PORT = dotenv.parsed.PORT || 3001
-const ENVIRON = dotenv.parsed.ENVIRON || 'DEV'
+const PORT = process.env.PORT || dotenv.parsed.PORT || 3001
+const ENVIRON = process.env.ENVIRON || dotenv.parsed.ENVIRON || 'DEV'
 const SERVER_HOST =
+  process.env.SERVER_HOST ||
   dotenv.parsed.SERVER_HOST ||
   (ENVIRON == 'DEV' ? `http://localhost:${PORT}/` : `https://wisedecider.herokuapp.com/`)
-const MONGODB_URI = dotenv.parsed.MONGODB_URI || `mongodb://localhost/nextjs-express-boilerplate`
+const MONGODB_URI =
+  process.env.MONGODB_URI ||
+  dotenv.parsed.MONGODB_URI ||
+  `mongodb://localhost/nextjs-express-boilerplate`
 
 module.exports = {
   ENVIRON,
